4b69a6846209d655b4153b9915b6eae51093a0c3
nsetyo <nsetyo.mail@gmail.com>
2016-05-02 14:20:57 +0700

    Update logo teks

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,9 +1,9 @@
 <!-- Logo -->
 <a href="{{ url('/') }}" class="logo">
   <!-- mini logo for sidebar mini 50x50 pixels -->
-  <span class="logo-mini"><b>A</b>LT</span>
+  <span class="logo-mini"><b>R</b>EN</span>
   <!-- logo for regular state and mobile devices -->
-  <span class="logo-lg"><b>Admin</b>LTE</span>
+  <span class="logo-lg"><b>Perencanaan</b></span>
 </a>
 <!-- Header Navbar: style can be found in header.less -->
 <nav class="navbar navbar-static-top" role="navigation">
@@ -11,7 +11,5 @@
   <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
     <span class="sr-only">Toggle navigation</span>
   </a>
-  <div class="navbar-custom-menu">
-    @include('includes.navbarmenu')
-  </div>
+  <div class="navbar-custom-menu"> @include('includes.navbarmenu') </div>
 </nav>
