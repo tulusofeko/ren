72b2b8862d7feb0c9d13f9d8906ba7b4c4c01557
nsetyo <nsetyo.mail@gmail.com>
2016-06-06 08:20:39 +0700

    Implementasi Node

--- app/Kegiatan.php
+++ app/Kegiatan.php
@@ -4,7 +4,44 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Kegiatan extends Model
+class Kegiatan extends Model implements Uraian
 {
-    //
+    protected $appends = array('state', 'parentId', 'mak');
+
+    /**
+     * Set the Programs's name.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = ucwords(strtolower($value));
+    }
+
+    public function getStateAttribute($value)
+    {
+        return 'closed';
+    }
+    
+    public function getParentIdAttribute($value)
+    {
+        return '051.01.' . $this->program;
+    }
+
+    public function getMakAttribute($value)
+    {
+        return $this->parentId . '.' . $this->code;
+    }
+
+    public function getChilds()
+    {
+        // return Kegiatan::where('program', $this->code)->get();
+    }
+
+    public function getChild($code)
+    {
+        // return Kegiatan::where([['program', $this->code], ['code', $code] ])
+            // ->firstOrFail();
+    }
 }
--- app/Program.php
+++ app/Program.php
@@ -4,8 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Program extends Model
+class Program extends Model implements Uraian
 {
+    protected $appends = array('state', 'parentId', 'mak');
+
     /**
      * Set the Programs's name.
      *
@@ -15,5 +17,31 @@ class Program extends Model
     public function setNameAttribute($value)
     {
         $this->attributes['name'] = ucwords(strtolower($value));
+    }
+
+    public function getStateAttribute($value)
+    {
+        return 'closed';
+    }
+    
+    public function getParentIdAttribute($value)
+    {
+        return '051.01';
+    }
+
+    public function getMakAttribute($value)
+    {
+        return '051.01.'. $this->code;
+    }
+
+    public function getChilds()
+    {
+        return Kegiatan::where('program', $this->code)->get();
+    }
+
+    public function getChild($code)
+    {
+        return Kegiatan::where([['program', $this->code], ['code', $code] ])
+            ->firstOrFail();
     }
 }
