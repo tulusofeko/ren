8d8c2c75c74b5f503c93a244d5533232d9277302
nsetyo <nsetyo.mail@gmail.com>
2016-06-17 13:09:42 +0700

    Revert

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Output;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,9 +19,6 @@ class AppServiceProvider extends ServiceProvider
         );
 
         view()->share('menus', $menus);
-
-        Output::creating(function ($output) { $output->setMak(); });
-
     }
 
     /**
