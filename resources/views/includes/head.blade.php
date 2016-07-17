c94e2160637d760c9c8afc1259e2f674345bf745
Setyo Nugroho <nsetyo.mail@gmail.com>
2016-07-17 10:24:48 +0700

    Ionicon dan fontawesome local

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -10,9 +10,9 @@
   <!-- Bootstrap 3.3.5 -->
   <link rel="stylesheet" href="{{ asset('adminlte/bootstrap/css/bootstrap.min.css') }}">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="{{ asset('font-awesome/css/font-awesome.min.css') }}">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="{{ asset('ionicons/css/ionicons.min.css') }}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('adminlte/dist/css/AdminLTE.min.css') }}">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
